72781240d9b06723ffa7b952cf57df21fc99f33f
Md. Farhan Shadiq <129365211+farhan-shadiq@users.noreply.github.com>
2024-11-02 13:14:30 +0600

    Update mailTest.m

--- mailTest.m
+++ mailTest.m
@@ -1,4 +1,4 @@
 clc;
 clear all;
 
-send_mail_message('shaem97@gmail.com', 'Comm project', 'This is a test email', 'Telemedicine.m');
+send_mail_message('farhan.eee15.buet@gmail.com', 'Comm project', 'This is a test email', 'Telemedicine.m');
